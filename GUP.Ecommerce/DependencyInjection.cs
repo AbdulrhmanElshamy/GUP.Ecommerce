03fe3ec19a5759e2df43a54a6a15221f2e59db4d
gedo <mustafaahmedelshaer@outlook.com>
2025-05-14 01:49:29 +0300

    register service

--- GUP.Ecommerce/DependencyInjection.cs
+++ GUP.Ecommerce/DependencyInjection.cs
@@ -58,6 +58,7 @@ public static class DependencyInjection
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<IDiscountService, DiscountService>();
+        services.AddScoped<ITapPaymentService, TapPaymentService>();
 
 
         services.AddExceptionHandler<GlobalExceptionHandler>();
